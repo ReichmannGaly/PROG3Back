02417200ffca9a7cbd0d267c0073347f061705d7
Jean Koletto Reichmann GALY <hei.reichmann.4@gmail.com>
2023-04-17 16:14:04 +0300

    first commit

---
+++ src/main/java/school/hei/prog3back/Prog3BackApplication.java
@@ -0,0 +1,13 @@
+package school.hei.prog3back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prog3BackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prog3BackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/school/hei/prog3back/Prog3BackApplicationTests.java
@@ -0,0 +1,13 @@
+package school.hei.prog3back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prog3BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
